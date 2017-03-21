fee2fbe69ee0d2edba52ed7149ea6b1e50ef6226
andypaipai <455274396@qq.com>
2017-03-21 16:15:07 +0800

    第一次提交

---
+++ testGit0100/testGit0100/src/com/testGit0100/model/TheOne.java
@@ -0,0 +1,9 @@
+package com.testGit0100.model;
+
+public class TheOne {
+
+	public static void main(String[] args) {
+		System.out.println("test GitHub.....");
+		System.out.println("test GitHub.....");
+	}
+}
